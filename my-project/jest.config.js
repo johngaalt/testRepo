9b3e3f221b457669fea7c5409fa82a99a4d4f706
johngaalt <rajon777666@gmail.com>
2024-04-30 14:57:35 +0300

    chore: coverage

--- my-project/jest.config.js
+++ my-project/jest.config.js
@@ -13,6 +13,14 @@ const config = {
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   setupFilesAfterEnv: ["<rootDir>/jest/jest-setup.ts"],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
 };
 
 export default config;
